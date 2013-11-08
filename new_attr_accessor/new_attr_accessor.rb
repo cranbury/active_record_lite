b04d84003c33867ee3ca754bc7a50f62344bff20
Granger <granger@appacademy.io>
2013-11-08 10:17:33 -0500

    did warm-up redoing attr_accessor

--- new_attr_accessor/new_attr_accessor.rb
+++ new_attr_accessor/new_attr_accessor.rb
@@ -1,2 +1,31 @@
-def new_attr_accessor
+class Class
+  def new_attr_accessor(*args)
+    #I'm going through each argument
+    args.each do |arg|
+
+      #Getter.
+      self.class_eval("def #{arg};@#{arg};end")
+
+      #setter
+      self.class_eval("def #{arg}=(val);@#{arg}=val;end")
+
+    end
+  end
 end
+
+
+# class MassObject
+#
+# end
+
+
+class Cat
+  new_attr_accessor :name, :color
+end
+
+> cat = Cat.new
+> cat.name = "Sally"
+> cat.color = "brown"
+
+> cat.name # => "Sally"
+> cat.color # => "brown"
